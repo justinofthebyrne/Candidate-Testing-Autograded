cd17ecfb19f9389804f8402de1402076cfd7d01a
justinofthebyrne <jpburnside3@gmail.com>
2024-01-21 16:19:27 -0600

    Correct Version: Tests Passed

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,17 +5,17 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = " ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = " ";
+let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
 let questions = ["Who was the first American woman in space? ", 
-"True or false: 5 kilometers === 5000 meters? ", 
-"(5+3)/2 * 10 = ? ", 
-"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
-"What is the minimum crew size for the ISS? "];
+                "True or false: 5 kilometer == 5000 meters? ", 
+                "(5 + 3)/2 * 10 = ? ",
+                "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+                "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"]; 
 let candidateAnswers = [];  
 
